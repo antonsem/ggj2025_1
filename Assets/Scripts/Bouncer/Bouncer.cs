319378cf9d9fa71a3c8c7d407eb128f0a12fdebf
ChristanBeyond <chrisjaver@gmail.com>
2025-01-25 14:41:00 +0100

    Initial version of bouncing off of a bouncer

--- Assets/Scripts/Bouncer/Bouncer.cs
+++ Assets/Scripts/Bouncer/Bouncer.cs
@@ -5,47 +5,22 @@ namespace BubbleHell
 {
     public class Bouncer : MonoBehaviour
     {
-        [SerializeField]
-        private float minVelocity = 10f;
-
-        private Vector3 lastFrameVelocity;
-        private Rigidbody rb;
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.TryGetComponent(out IBounceable bounceable))
-            {
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (other.gameObject.TryGetComponent(out IBounceable bounceable))
-            {
-            }
-        }
-
-        private void OnEnable()
-        {
-            rb = GetComponent<Rigidbody>();
-        }
-
-        private void Update()
-        {
-            lastFrameVelocity = rb.linearVelocity;
-        }
+        [SerializeField] private float bounceForce = 10f;
 
         private void OnCollisionEnter(Collision collision)
         {
-            Bounce(collision.contacts[0].normal);
+            if (collision.transform.TryGetComponent(out IBounceable bounceable))
+            {
+                bounceable.SetSpeed(bounceForce, CalculateBounceVector(bounceable.Velocity, collision.contacts[0].normal));
+            }
         }
 
-        private void Bounce(Vector3 collisionNormal)
+        private Vector3 CalculateBounceVector(Vector3 velocity, Vector3 collisionNormal)
         {
-            var speed = lastFrameVelocity.magnitude;
-            var direction = Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
+            var speed = velocity.magnitude;
+            var direction = Vector3.Reflect(velocity.normalized, collisionNormal);
 
-            rb.linearVelocity = direction * Mathf.Max(speed, minVelocity);
+            return direction * speed;
         }
     }
 }
