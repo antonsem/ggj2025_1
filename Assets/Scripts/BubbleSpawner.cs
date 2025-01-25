295dcefd5eccb06b93d8579374da7f7c7c47b029
ChristanBeyond <chrisjaver@gmail.com>
2025-01-25 13:21:34 +0100

    Updated the spawn script to allow spawning in pickupfield

--- Assets/Scripts/BubbleSpawner.cs
+++ Assets/Scripts/BubbleSpawner.cs
@@ -8,22 +8,27 @@ namespace BubbleHell
         [SerializeField] private GameObject bubble;
         [SerializeField] private Transform corner1, corner2;
         [SerializeField] private Transform bubbleParent;
+        [SerializeField] private Transform pickupField;
 
         void Update()
         {
-            if(Input.GetKeyDown(KeyCode.E))
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                SpawnBubble(pickupField);
+            }
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 SpawnBubble();
             }
         }
 
-        private void SpawnBubble()
+        private void SpawnBubble(Transform spawnPos = null)
         {
             float randX = Random.Range(corner1.position.x, corner2.position.x);
             float randZ = Random.Range(corner1.position.z, corner2.position.z);
             Vector3 randPos = new(randX, 1, randZ);
-    
-            Instantiate(bubble, randPos,  Quaternion.identity, bubbleParent);
+
+            Instantiate(bubble, spawnPos == null ? randPos : spawnPos.position, Quaternion.identity, bubbleParent);
         }
     }
 }
