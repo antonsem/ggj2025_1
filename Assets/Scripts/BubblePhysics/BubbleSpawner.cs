2dccc79272c0da37149a4b36290899d6e0244540
ChristanBeyond <chrisjaver@gmail.com>
2025-01-25 16:03:03 +0100

    Spawner now conditionally spawns
    It will not spawn inside colliders.

--- Assets/Scripts/BubblePhysics/BubbleSpawner.cs
+++ Assets/Scripts/BubblePhysics/BubbleSpawner.cs
@@ -7,6 +7,14 @@ namespace BubbleHell
     {
         [SerializeField] private GameObject bubble;
         [SerializeField] private Transform ground;
+        private float _sphereCastRadius;
+        private int _layerMask;
+
+        private void Awake()
+        {
+            _sphereCastRadius = bubble.GetComponent<SphereCollider>().radius;
+            _layerMask = ~(1 << LayerMask.NameToLayer("Ground"));
+        }
 
         void Update()
         {
@@ -32,8 +40,12 @@ namespace BubbleHell
             float randX = Random.Range(groundCenter.x - groundCenterScale.x, groundCenter.x + groundCenterScale.x);
             float randZ = Random.Range(groundCenter.z - groundCenterScale.z, groundCenter.z + groundCenterScale.z);
             Vector3 randPos = new(randX, groundCenterScale.y * 2, randZ);
-
-            Instantiate(bubble, randPos, Quaternion.identity, transform);
+    
+            Collider[] hitColliders = Physics.OverlapSphere(randPos, _sphereCastRadius, _layerMask);
+            if (hitColliders.Length == 0)
+            {
+                Instantiate(bubble, randPos, Quaternion.identity, transform);
+            }
         }
 
         private void PurgeBubbles()
