f7c9eb814d23b876be986e9c13c3554c4e8b9121
Anton Semchenko <anton.semchenko@beyondsports.nl>
2025-01-26 12:25:20 +0100

    - destroying particles

---
+++ Assets/Scripts/Misc/DestroyTimer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace BubbleHell.Misc
+{
+	public class DestroyTimer : MonoBehaviour
+	{
+		[SerializeField] private float _destroyAfter;
+
+		private float _startTime;
+
+		private void Start()
+		{
+			_startTime = Time.time;
+		}
+
+		private void Update()
+		{
+			if(Time.time - _startTime > _destroyAfter)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
