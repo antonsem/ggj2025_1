ea1db6fc58086a57047fd3f17c3e665d245bdb58
Anton Semchenko <anton.semchenko@beyondsports.nl>
2025-01-26 13:10:28 +0100

    - added spawn in animation and particles

---
+++ Assets/Scripts/BubblePhysics/BubbleInEffect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace BubbleHell.BubblePhysics
+{
+	public class BubbleInEffect : MonoBehaviour
+	{
+		[SerializeField] private Transform _visuals;
+		[SerializeField] private AnimationCurve _popInCurve;
+		[SerializeField] private float _speed;
+		
+
+		private void Awake()
+		{
+			_visuals.localScale = Vector3.zero;
+		}
+
+		private IEnumerator Start()
+		{
+			float t = 0;
+
+			while (t < 1)
+			{
+				_visuals.localScale = Vector3.one * _popInCurve.Evaluate(t);
+				t += Time.deltaTime * _speed;
+				yield return null;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/BubblePhysics/BubbleSpawnEffect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BubbleHell.BubblePhysics
+{
+	public class BubbleSpawnEffect : MonoBehaviour
+	{
+		[SerializeField] private GameObject _spawnEffectPrefab;
+		[SerializeField] private BubbleSpawner _bubbleSpawner;
+
+		private readonly List<GameObject> _spawnedEffects = new();
+
+		private void Awake()
+		{
+			_bubbleSpawner.OnStartSpawn += OnBubbleSpawn;
+		}
+
+		private void OnDestroy()
+		{
+			foreach (GameObject spawnedEffect in _spawnedEffects)
+			{
+				spawnedEffect.GetComponent<ParticleSystem>().Stop();
+				Destroy(spawnedEffect.gameObject);
+			}
+
+			_spawnedEffects.Clear();
+		}
+
+		private void OnBubbleSpawn(Vector3 position)
+		{
+			foreach (GameObject spawnedEffect in _spawnedEffects)
+			{
+				if(!spawnedEffect.activeSelf)
+				{
+					spawnedEffect.transform.position = position;
+					spawnedEffect.SetActive(true);
+					spawnedEffect.GetComponent<ParticleSystem>().Stop();
+					spawnedEffect.GetComponent<ParticleSystem>().Play();
+					return;
+				}
+			}
+
+			GameObject newEffect = Instantiate(_spawnEffectPrefab, position, Quaternion.identity);
+			_spawnedEffects.Add(newEffect);
+		}
+	}
+}
